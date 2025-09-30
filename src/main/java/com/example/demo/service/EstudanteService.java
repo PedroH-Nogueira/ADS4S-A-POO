17fed30cfca359762e7d3338658b14eea560c7df
unknown <pedrinhonoguera12@gmail.com>
2025-09-29 22:26:24 -0300

    ServiceCriado

--- src/main/java/com/example/demo/service/EstudanteService.java
+++ src/main/java/com/example/demo/service/EstudanteService.java
@@ -1,4 +1,31 @@
 package com.example.demo.service;
+import com.example.demo.model.Estudante;
+import com.example.demo.repository.EstudanteRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class EstudanteService {
+    private final EstudanteRepository estudanteRepository;
+
+    public EstudanteService(EstudanteRepository estudanteRepository) {
+        this.estudanteRepository = estudanteRepository;
+    }
+
+    public List<Estudante> getAll(){
+        return estudanteRepository.findAll();
+    }
+
+    public Estudante getById(Long id){
+        return estudanteRepository.findById(id).orElseThrow();
+    }
+
+    public Estudante save(Estudante estudante){
+        return estudanteRepository.save(estudante);
+    }
+
+    public void delete(Long id){
+        estudanteRepository.deleteById(id);
+    }
 }
