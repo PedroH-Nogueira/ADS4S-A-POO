9d4c8947ed21079348de9e88f8afc85d0005961d
unknown <pedrinhonoguera12@gmail.com>
2025-09-29 22:24:10 -0300

    RepositoryCriado

--- src/main/java/com/example/demo/repository/EstudanteRepository.java
+++ src/main/java/com/example/demo/repository/EstudanteRepository.java
@@ -1,4 +1,8 @@
 package com.example.demo.repository;
+import com.example.demo.model.Estudante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public class EstudanteRepository {
+@Repository
+public interface EstudanteRepository extends JpaRepository<Estudante, Long> {
 }
